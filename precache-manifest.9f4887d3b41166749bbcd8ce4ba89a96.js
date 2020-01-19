1b083326afb7632c818831682dfb6eb1ba7bfced
Isuru Uthpala <isuruuthpalaiup@gmail.com>
2020-01-19 13:02:46 +0530

    Updates

---
+++ precache-manifest.9f4887d3b41166749bbcd8ce4ba89a96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90db5b215541632dab9703764a811e97",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "0191d008d5119cd3fb24",
+    "url": "/monster/static/css/main.0842de7f.chunk.css"
+  },
+  {
+    "revision": "b8d56c54a1a88f421f72",
+    "url": "/monster/static/js/2.66b6756b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster/static/js/2.66b6756b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0191d008d5119cd3fb24",
+    "url": "/monster/static/js/main.fe1c3b19.chunk.js"
+  },
+  {
+    "revision": "3eb73c55e45d74d3c175",
+    "url": "/monster/static/js/runtime-main.bf55bddf.js"
+  }
+]);
